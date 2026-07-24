ba10431566f8bb4ca1f211ef54021db76ce78358
Oumer Adem <oumerahmed0@gmail.com>
2026-07-24 00:20:10 -0700

    Navbar and profilie update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ export const Navbar = ({menuOpen,setMenuOpen}) => {
             <div className="flex justify-between items-center h-16">
                 <a href="#home" className="font-mono text-xl font-bold text-white">
                  {"  "}
-                oumer<span className="text-blue-500">.tech</span>{"  "}
+                oumer<span className="text-blue-500">adem.com</span>{"  "}
                 </a>
 
                <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden" 
